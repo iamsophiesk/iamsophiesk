354fa335d5946759397dee45eea97267baa74261
Sophie Shanahan-Kluth <sophie.sk@focusrite.com>
2019-07-18 16:52:06 +0100

    Restructuring CSS, adding pretty things.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const glob = require('node-sass-globbing');
+
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const concat = require('gulp-concat');
+const uglify = require('gulp-terser');
+const optimizejs = require('gulp-optimize-js');
 
 // Generate CSS from Sass files.
-gulp.task('css', function() {
+gulp.task('css', function () {
   return gulp.src('./src/scss/main.scss')
     .pipe(sass({
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
+      importer: glob
     })
     .on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
+// Do fancy JavaScript stuff.
+gulp.task('js', function () {
+  return gulp.src('./src/js/*')
+    .pipe()
+});
+
 // Watch folders for changes.
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('./src/scss/**/*.scss', gulp.parallel('css'));
 });
 
---
+++ src/js/code-language.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Add code language as content to the start of a code block.
+ */
+
+
+
+ var elements = document.querySelectorAll(selector);
+ Array.prototype.forEach.call(elements, function(el, i) => {
+
+ });
